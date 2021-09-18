10f502ce8167b03f33b203cedfd66437c3f94b3a
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2021-09-18 13:45:33 +0300

    style: add classNames to PageNotFound component

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 function PageNotFound () {
   return (
-    <div className="">
-      <h3 className="">
+    <div className="not-found">
+      <h3 className="not-found__title">
        <span>404</span> - Страница не найдена
       </h3>
-      <p className="">
+      <p className="not-found__subtitle">
        Ой, здесь ничего нет
       </p>
-      <Link className="button button_type_to-main" to="/">Назад</Link>
+      <Link className="page__buttons" to="/">Назад</Link>
     </div>
   )
 }
