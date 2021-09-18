9b1794b4206977093d422b5f6071a091e7711dfc
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2021-09-18 03:05:49 +0300

    feat: start input validation in EditAvatarPopup component

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,8 +1,38 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useCallback, useEffect } from 'react';
 import PopupWithForm from './PopupWithForm';
 
 function EditAvatarPopup(props) {
   const avatarRef = useRef('');
+
+  const [formValues, setFormValues] = useState({
+    avatar: '',
+  });
+
+  const [formValidity, setFormValidity] = useState({
+    avatarValid: false,
+  })
+
+  const handleInputChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setFormValues(prevState => ({ ...prevState, [name]: value}));
+  }, [setFormValues]);
+
+  useEffect(function validateInputs() {
+    const isAvatarFilled = formValues.avatar.length > 10;
+    const isAvatarValid = isAvatarFilled;
+
+    setFormValidity(prevValidity => ({
+      avatarValid: isAvatarValid,
+    }));
+  }, [formValues, setFormValidity]);
+
+  const { avatar } = formValues;
+  console.log(formValues);
+
+  const { avatarValid } = formValidity;
+
+  //передать кнопке: disabled={isSubmitDisabled}
+  //const isSubmitDisabled = !avatarValid;
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -14,7 +44,8 @@ function EditAvatarPopup(props) {
 
   return(
     <PopupWithForm name="userPhotoForm" title="Обновить аватар" isOpen={props.isOpen} onClose={props.onClose} onSubmit={handleSubmit} textOnButton="Сохранить">
-      <input id="url" type="url" ref={avatarRef} className="input-container__item" name="avatar" placeholder="Ссылка на фото" defaultValue="" tabIndex="1" required />
+      <input id="url" type="url" ref={avatarRef} className="input-container__item" name="avatar" placeholder="Ссылка на фото" value={avatar} tabIndex="1" required onChange={handleInputChange} />
+      {!avatarValid && <div>Введите корректную ссылку</div>}
       <span className="input-container__input-error" id="url-error" />
     </PopupWithForm>
   )
